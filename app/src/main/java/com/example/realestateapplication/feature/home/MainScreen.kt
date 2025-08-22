f7a5a9bc1814814cee83119b5bfe714e9746246c
kimiti <kimitikevin@gmail.com>
2025-08-22 14:53:40 +0300

    adds app home screen

--- app/src/main/java/com/example/realestateapplication/feature/home/MainScreen.kt
+++ app/src/main/java/com/example/realestateapplication/feature/home/MainScreen.kt
@@ -1,2 +1,30 @@
 package com.example.realestateapplication.feature.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.realestateapplication.R
+import com.example.realestateapplication.ui.components.HeaderSection
+
+@Composable
+@Preview
+fun MainScreen() {
+    val items = remember { sampleProperties() }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .background(color = colorResource(R.color.light_grey)),
+        contentPadding = PaddingValues(bottom = 100.dp)
+    ) {
+        item { HeaderSection() }
+    }
+}
